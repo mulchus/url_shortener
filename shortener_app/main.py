b33934712b8eb072dc801efca77aad527d7b684b
mulchus <mulch@list.ru>
2024-06-19 17:35:42 +0300

    add forward a shortened URL

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,7 +1,8 @@
 import secrets
 
 import validators
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
 from . import schemas, models
@@ -19,13 +20,35 @@ def get_db():
         db.close()
 
 
+def raise_not_found(request):
+    message = f"URL '{request.url}' doesn't exist"
+    raise HTTPException(status_code=404, detail=message)
+
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
+
 @app.get("/")
 def read_root():
     return "Welcome to the URL shortener API :)"
 
 
-def raise_bad_request(message):
-    raise HTTPException(status_code=400, detail=message)
+@app.get("/{url_key}")
+def forward_to_target_url(
+        url_key: str,
+        request: Request,
+        db: Session = Depends(get_db)
+    ):
+    db_url = (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+    if db_url:
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
 
 
 @app.post("/url", response_model=schemas.URLInfo)
@@ -42,8 +65,7 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     db.add(db_url)
     db.commit()
     db.refresh(db_url)
-    # зачем дублируется присваивание ниже?
-    # db_url.url = key
-    # db_url.admin_url = secret_key
+    db_url.url = key
+    db_url.admin_url = secret_key
 
     return db_url
